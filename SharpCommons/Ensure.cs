80c4ed8418d185ed95a090be1f3e96334901314f
RL <li.rongcheng@gmail.com>
2016-09-29 18:18:03 +1000

    add ultility lib projct SharpCommon

---
+++ SharpCommons/Ensure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCommons
+{
+    static public class Ensure
+    {
+        private static void ThrowException(string msg = "")
+        {
+            throw new Exception("Ensure confirmation failed. " + msg);
+        }
+
+        public static void IsTrue(bool condition)
+        {
+            if (!condition)
+            {
+                ThrowException();
+            }
+        }
+
+        public static void IsTrue(bool condition, string msg)
+        {
+            if (!condition)
+            {
+                ThrowException(msg);
+            }
+        }
+    }
+}
---
+++ SharpCommons/SafeCaller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCommons
+{
+    public class SafeCall
+    {
+        static public void ExecuteWithError(Action fn)
+        {
+            try
+            {
+                fn();
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteError(ex.Message);
+                Trace.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static public void ExecuteWithWarning(Action fn)
+        {
+            try
+            {
+                fn();
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteWarning(ex.Message);
+                Trace.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
