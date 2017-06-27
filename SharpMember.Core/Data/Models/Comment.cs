278f5a81e804758934437c412b7b3ce4fc051930
Ron <cynosure.veda@gmail.com>
2017-06-28 00:36:23 +1000

    Add EF entity: Comment

---
+++ SharpMember.Core/Data/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMember.Core.Data.Models
+{
+    public class CommentEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class Comment : CommentEntity
+    {
+        public virtual WorkTask WorkTask { get; set; }
+    }
+}
--- SharpMember.Core/Data/Models/WorkTask.cs
+++ SharpMember.Core/Data/Models/WorkTask.cs
@@ -12,12 +12,12 @@ namespace SharpMember.Core.Data.Models
         public bool Done { get; set; }
         public bool Pinned { get; set; }
         public string Status { get; set; }  // user input or select from a drop down menu
-        public string Comments { get; set; }
     }
 
     public class WorkTask : WorkTaskEntity
     {
         public virtual List<CheckListItem> CheckListItems { get; set; } = new List<CheckListItem>();
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
         public virtual MemberProfile WorkTaskCreator { get; set; }
         public virtual MemberProfile WorkTaskOwner { get; set; }
         public virtual ApplicationUser ApplicationUser { get; set; }    // for user's private tasks
