205bc395651da29d84074e0bb8fdbbe567083f02
Ron <cynosure.veda@gmail.com>
2017-07-06 16:38:14 +1000

    Add poll entity in activity system

---
+++ SharpMember.Core/Data/Models/ActivitySystem/Poll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMember.Core.Data.Models.ActivitySystem
+{
+    public class PollEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Poll : PollEntity
+    {
+        public virtual List<PollOption> PollOptions { get; set; }
+    }
+}
---
+++ SharpMember.Core/Data/Models/ActivitySystem/PollOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMember.Core.Data.Models.ActivitySystem
+{
+    public class PollOptionEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Votes { get; set; }
+    }
+
+    public class PollOption : PollOptionEntity
+    {
+    }
+}
