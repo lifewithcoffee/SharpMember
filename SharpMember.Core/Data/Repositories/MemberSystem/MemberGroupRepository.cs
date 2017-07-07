01b4bc22cfb8eb66b871bd30f8d5565d4cf21dcf
Ron <cynosure.veda@gmail.com>
2017-07-07 15:02:49 +1000

    Add and implement method IMemberGroupRepository.AddAsync()

--- SharpMember.Core/Data/Repositories/MemberSystem/MemberGroupRepository.cs
+++ SharpMember.Core/Data/Repositories/MemberSystem/MemberGroupRepository.cs
@@ -5,15 +5,46 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using SharpMember.Core.Data.Models.MemberSystem;
+using System.Threading.Tasks;
 
 namespace SharpMember.Core.Data.Repositories.MemberSystem
 {
-    public interface IMemberGroupRepository : IRepositoryBase<MemberGroup, ApplicationDbContext> { }
+    public interface IMemberGroupRepository : IRepositoryBase<MemberGroup, ApplicationDbContext>
+    {
+        Task<MemberGroup> AddAsync(int orgId, string memberGroupName);
+    }
 
     public class MemberGroupRepository : RepositoryBase<MemberGroup, ApplicationDbContext>, IMemberGroupRepository
     {
-        public MemberGroupRepository(IUnitOfWork<ApplicationDbContext> unitOfWork, ILogger logger) : base(unitOfWork, logger)
+        private readonly IOrganizationRepository _organizationRepository;
+
+        public MemberGroupRepository(
+            IUnitOfWork<ApplicationDbContext> unitOfWork
+            , ILogger logger
+            , IOrganizationRepository organizationRepository
+        ) : base(unitOfWork, logger)
         {
+            this._organizationRepository = organizationRepository;
+        }
+
+        /// <summary>
+        /// Precondition:
+        /// - the organization must exist
+        /// - the MemberGroup with the specified name must NOT exist
+        /// </summary>
+        public async Task<MemberGroup> AddAsync(int orgId, string memberGroupName)
+        {
+            if (!await _organizationRepository.ExistAsync(o => o.Id == orgId))
+            {
+                throw new OrganizationNotExistException();
+            }
+
+            if (await this.ExistAsync(m => m.Name == memberGroupName))
+            {
+                throw new MemberNameExistException();
+            }
+
+            return base.Add(new MemberGroup { Name = memberGroupName });
         }
     }
 }
---
+++ SharpMember.Core/Exceptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMember.Core
+{
+    public class MemberNameExistException : Exception { }
+
+    public class OrganizationNotExistException : Exception { }
+}
