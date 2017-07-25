cf4e80ba3244f6204300f71200f3bf0a3e3c65ac
Ron <cynosure.veda@gmail.com>
2017-07-26 01:10:50 +1000

    Update unit test for OrganizationRepository

---
+++ SharpMember.XUnitTest/DataRepositories/OrganizationRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SharpMember.Core;
+using SharpMember.Core.Data;
+using SharpMember.Core.Data.Models.MemberSystem;
+using SharpMember.Core.Data.Repositories.MemberSystem;
+using SharpMember.Core.Data.RepositoryBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace U.DataRepositories
+{
+    public class OrganizationTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            string configureFileDir = $"TestGlobalSettings\\SharpMember";
+            IConfigurationRoot Configuration = new ConfigurationBuilder()
+                .SetBasePath(TestGlobalSettings.sharpMemberProjectPath)
+                .AddJsonFile(TestGlobalSettings.sharpMemberJsonSettingName)
+                .Build();
+
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSharpMemberCore(Configuration);
+            serviceCollection.AddTransient<ILogger>(f => new Mock<ILogger>().Object);   // TODO: make an MCN notes here
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            IOrganizationRepository repo = serviceProvider.GetService<IOrganizationRepository>();
+
+            Assert.NotNull(repo);
+
+            repo.Add(new Organization { Name = Guid.NewGuid().ToString()});
+            repo.Commit();
+        }
+    }
+}
---
+++ SharpMember.XUnitTest/TestGlobalSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace U
+{
+    public class TestGlobalSettings
+    {
+        public static string solutionRoot = @"F:\rp\git\working\SharpMember";
+        public static string sharpMemberProjectPath = $@"{solutionRoot}\SharpMember";
+        public static string sharpMemberJsonSettingName = "appsettings.json";
+    }
+}
