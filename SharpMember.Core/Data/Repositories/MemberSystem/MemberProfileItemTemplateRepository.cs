5002b6b221380bc4d863432ac47df52ebb45b67c
Ron <cynosure.veda@gmail.com>
2017-07-26 15:07:38 +1000

    Add MemberProfileItemTemplateRepository

---
+++ SharpMember.Core/Data/Repositories/MemberSystem/MemberProfileItemTemplateRepository.cs
@@ -0,0 +1,15 @@
+﻿using SharpMember.Core.Data.RepositoryBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace SharpMember.Core.Data.Repositories.MemberSystem
+{
+    public interface IMemberProfileItemTemplateRepository : IRepositoryBase<MemberProfileItemTemplate, ApplicationDbContext> { }
+
+    public class MemberProfileItemTemplateRepository : RepositoryBase<MemberProfileItemTemplate, ApplicationDbContext>, IMemberProfileItemTemplateRepository
+    {
+        public MemberProfileItemTemplateRepository(IUnitOfWork<ApplicationDbContext> unitOfWork, ILogger logger) : base(unitOfWork, logger) { }
+    }
+}
