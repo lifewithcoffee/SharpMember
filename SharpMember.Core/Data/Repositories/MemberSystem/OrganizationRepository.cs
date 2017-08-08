2ff0b2a7ff0948bdffd4c3b5f295144c6ab27cfa
Ron <cynosure.veda@gmail.com>
2017-08-08 11:40:50 +1000

    Update OrganizationRepository

--- SharpMember.Core/Data/Repositories/MemberSystem/OrganizationRepository.cs
+++ SharpMember.Core/Data/Repositories/MemberSystem/OrganizationRepository.cs
@@ -6,21 +6,56 @@ using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Linq.Expressions;
 using SharpMember.Core.Data.Models.MemberSystem;
+using SharpMember.Core.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace SharpMember.Core.Data.Repositories.MemberSystem
 {
     public interface IOrganizationRepository : IRepositoryBase<Organization, ApplicationDbContext>
     {
         Organization Add(string name);
+        Task<Organization> CreateAsync(string appUserId, string name);
+        Member RegisterMember(string appUserId);
+        void CancelMember(string appUserId);
     }
 
     public class OrganizationRepository : RepositoryBase<Organization, ApplicationDbContext>, IOrganizationRepository
     {
-        public OrganizationRepository(IUnitOfWork<ApplicationDbContext> unitOfWork, ILogger logger) : base(unitOfWork, logger) { }
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public OrganizationRepository(
+            IUnitOfWork<ApplicationDbContext> unitOfWork
+            , ILogger logger
+            , UserManager<ApplicationUser> userManager
+        ) : base(unitOfWork, logger)
+        {
+            this._userManager = userManager;
+        }
 
         public Organization Add(string name)
         {
             return this.Add(new Organization { Name = name });
+        }
+
+        public async Task<Organization> CreateAsync(string appUserId, string name)
+        {
+            ApplicationUser appUser = await this._userManager.FindByIdAsync(appUserId);
+            Member newMember = this.RegisterMember(appUserId);
+            Organization org = new Organization { Name = name };
+
+            org.Members.Add(newMember);
+            throw new NotImplementedException();
+        }
+
+        public Member RegisterMember(string appUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelMember(string appUserId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
