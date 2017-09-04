94d135b0a5442cbdd33d3f3a237926ce7796598c
Ron <cynosure.veda@gmail.com>
2017-09-04 22:36:36 +1000

    When add new item templates from the community setting, member edit page should also show the newly added items

--- SharpMember.Core/Views/ViewServices/MemberViewService.cs
+++ SharpMember.Core/Views/ViewServices/MemberViewService.cs
@@ -74,12 +74,24 @@ namespace SharpMember.Core.Views.ViewServices
         {
             MemberUpdateVM result = null;
 
+            // get existing items
             var member = await _memberRepository.GetMany(m => m.Id == id).Include(m => m.MemberProfileItems).SingleOrDefaultAsync();
             if (member != null)
             {
                 result = MemberMapper<Member, MemberUpdateVM>.Cast(member);
                 result.ProfileItemViewModels = await ConvertTo.MemberProfileItemVMList(member.MemberProfileItems, _memberProfileItemTemplateRepository);
             }
+
+            // get the possible new items from the community item templates
+            var templateIds = member.MemberProfileItems.Select(i => i.MemberProfileItemTemplateId).ToList();
+            var newItems = _memberProfileItemTemplateRepository
+                .GetMany(t => t.CommunityId == member.CommunityId)
+                .Where(t => !templateIds.Contains(t.Id))
+                .Select(t => new MemberProfileItem { MemberId = member.Id, MemberProfileItemTemplateId = t.Id })
+                .ToList();
+
+            var newItemVms = await ConvertTo.MemberProfileItemVMList(newItems, _memberProfileItemTemplateRepository);
+            result.ProfileItemViewModels.AddRange(newItemVms);
 
             return result;
         }
