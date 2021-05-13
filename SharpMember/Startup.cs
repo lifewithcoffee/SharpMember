ea91290d211619a6b943b0ce371957738e6e4c04
li-rongcheng <Li.Rongcheng@gmail.com>
2021-05-14 02:57:41 +1000

    upgrade to .net5

--- SharpMember/Startup.cs
+++ SharpMember/Startup.cs
@@ -93,8 +93,8 @@ namespace SharpMember
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
