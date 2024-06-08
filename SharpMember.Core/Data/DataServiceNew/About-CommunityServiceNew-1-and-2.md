f526f77fa5ce5eef724a5b6a9b2167990f404f19
Ron <li.rongcheng@gmail.com>
2024-06-09 03:35:54 +1000

    Update for new CommunityService implementation

---
+++ SharpMember.Core/Data/DataServiceNew/About-CommunityServiceNew-1-and-2.md
@@ -0,0 +1,27 @@
+﻿# About CommunityServiceNew1/2
+
+## 2 possible ways to implement DDD aggregate
+
+CommunityServiceNew1 and CommunityServiceNew2 are 2 experimental approaches of
+implementing DDD aggregate root:
+
+- CommunityServiceNew1 is based on the `IRepository` of package `NetCoreUtils.Database`
+- CommunityServiceNew2 is based on a dedicated DbContext
+
+## DbContext per aggregate root (for CommunityServiceNew2)
+
+Thought 2024-05-25:
+
+- Every repository (named with the aggregate root object) is a DbContext with a subset
+  of DbSet
+  
+- May need to design a dedicated BaseDbContext for ChangeTracker.AutoDetectChangesEnabled
+  handling, logging etc.
+  
+- The repository DbContext will never be used for migration (migration will only be done
+  via a 'overall' DbContext containing all DbSets
+  
+- The repository may have multiple DB schema, i.e. the repository is not designed by DB
+  schema
+  
+- The repository will be rejected into a relevant service
--- SharpMember.Core/Data/DataServiceNew/CommunityServiceNew1.cs
+++ SharpMember.Core/Data/DataServiceNew/CommunityServiceNew1.cs
@@ -11,25 +11,7 @@ using System.Threading.Tasks;
 
 namespace SharpMember.Core.Data.DataServiceNew
 {
-    /// <summary>
-    /// ICommunityServiceNew merges the old IMemberService and ICommunityService, as
-    /// the "Community" object should be used as the aggregate root of the member system
-    /// </summary>
-    interface ICommunityServiceNew : IBindable<ICommunityServiceNew>
-    {
-        // from old IMemberService
-        int GetNextUnassignedMemberNumber();
-        IQueryable<Member> QueryMemberByNumber(int memberNumber);   // renamed from GetByMemberNumber()
-        IQueryable<Member> QueryMembers();    // renamed from GetByCommunity()
-        Task<Member> CreateMemberAsync(string appUserId);  // renamed from GenerateNewMemberWithProfileItemsAsync()
-
-        // from old ICommunityService
-        Task<Member> AddMemberAsync(string appUserId, string name, string email, string role);
-        Task AddMemberProfileTemplateAsync(string itemName, bool required);
-        Task CreateCommunityAsync(string appUserId, string communityName);
-    }
-
-    internal class CommunityServiceNew : ICommunityServiceNew
+    public class CommunityServiceNew1 : ICommunityServiceNew
     {
         int _communityId;
 
