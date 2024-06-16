65edc204e25968884d97ca406597658cafad238f
Ron <li.rongcheng@gmail.com>
2024-06-17 02:23:18 +1000

    Add Forum, update Meeting

--- SharpMember.Core/Data/Models/Meeting/Meeting.cs
+++ SharpMember.Core/Data/Models/Meeting/Meeting.cs
@@ -7,9 +7,11 @@ namespace SharpMember.Core.Data.Models.Meeting
 {
     public class MeetingEntity
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public DateTime? DateTime { get; set; }
+        public decimal PlannedHours { get; set; }
+        public string OnlineLink { get; set; }
         public string Address { get; set; }
         public string Details { get; set; }
     }
